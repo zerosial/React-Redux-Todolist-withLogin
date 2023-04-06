3229733a07d95372b0748eb96c7f94b033103565
Sunghum Paik <zerosial@gmail.com>
2023-04-06 20:31:03 +0900

    feat: 로그인 작업완료

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,9 +1,104 @@
-import { Container, Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Input,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const SignIn = () => {
+  const [userInput, setUserInput] = useState({
+    isDisabled: true,
+    email: false,
+    password: false,
+  });
+
+  const formHandler = (e: any) => {
+    e.preventDefault();
+    const email = (e.currentTarget.elements[0] as HTMLInputElement).value;
+    const password = (e.currentTarget.elements[1] as HTMLInputElement).value;
+    // api 연결
+  };
+
+  const emailValueHandler = (e: any) => {
+    const email = String(e.target.value);
+    if (email.includes("@")) {
+      setUserInput((prevState) => {
+        return { ...prevState, email: true };
+      });
+      if (userInput.password) {
+        setUserInput((prevState) => {
+          return { ...prevState, isDisabled: false };
+        });
+      }
+    } else {
+      setUserInput((prevState) => {
+        return { ...prevState, email: false, isDisabled: true };
+      });
+    }
+  };
+
+  const passwordValueHandler = (e: any) => {
+    const password = String(e.target.value);
+    if (password.length >= 8) {
+      setUserInput((prevState) => {
+        return { ...prevState, password: true };
+      });
+      if (userInput.email) {
+        setUserInput((prevState) => {
+          return { ...prevState, isDisabled: false };
+        });
+      }
+    } else {
+      setUserInput((prevState) => {
+        return { ...prevState, password: false, isDisabled: true };
+      });
+    }
+  };
+
   return (
-    <Container maxW={"100%"} p={"16px"}>
-      <Heading>SignIn</Heading>
+    <Container maxW={"50%"}>
+      <Grid justifyItems="center">
+        <FormControl>
+          <form onSubmit={formHandler}>
+            <FormLabel>Email address</FormLabel>
+            <Input
+              type="email"
+              data-testid="email-input"
+              onChange={emailValueHandler}
+            />
+            <FormHelperText>이메일을 입력해 주세요</FormHelperText>
+            <FormLabel>Password</FormLabel>
+            <Input
+              type="password"
+              data-testid="password-input"
+              onChange={passwordValueHandler}
+            />
+            <FormHelperText mb={6}>
+              비밀번호는 8자리 이상 입력해주세요
+            </FormHelperText>
+            <Button
+              type="submit"
+              mb={6}
+              data-testid="signin-button"
+              w={80}
+              isDisabled={userInput.isDisabled}
+            >
+              로그인
+            </Button>
+          </form>
+        </FormControl>
+        <Divider mb={6} />
+        <Text color="gray.300" mb={6}>
+          혹시 회원가입이 아직이신가요?
+        </Text>
+        <Button w={80}>회원가입</Button>
+      </Grid>
     </Container>
   );
 };
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,9 +1,11 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Button, Container, Input } from "@chakra-ui/react";
 
 const SighUp = () => {
   return (
     <Container maxW={"100%"} p={"16px"}>
-      <Heading>SighUp</Heading>
+      <Input data-testid="email-input" />
+      <Input data-testid="password-input" />
+      <Button data-testid="signup-button">회원가입</Button>
     </Container>
   );
 };
