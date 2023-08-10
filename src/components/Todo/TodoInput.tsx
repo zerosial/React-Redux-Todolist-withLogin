72586a34837da323292e98d167f20b9460c70f82
Sunghum Paik (Brian) <zerosial@gmail.com>
2023-08-10 14:04:01 +0900

    feat: 추가 버튼 작동에러 수정

--- src/components/Todo/TodoInput.tsx
+++ src/components/Todo/TodoInput.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Input } from "@chakra-ui/react";
+import { Flex, Input } from "@chakra-ui/react";
 import { TodoInputProps } from "types/Todo";
 
 const TodoInput = ({ onAddTodo }: TodoInputProps) => {
@@ -6,7 +6,7 @@ const TodoInput = ({ onAddTodo }: TodoInputProps) => {
     <form onSubmit={onAddTodo}>
       <Flex gap={4} mb={8}>
         <Input data-testid="new-todo-input" w={80} />
-        <Button data-testid="new-todo-add-button">추가</Button>
+        <button data-testid="new-todo-add-button">추가</button>
       </Flex>
     </form>
   );
